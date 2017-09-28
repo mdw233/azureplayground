2e47fe56b3988696d55c13bdde979c226b7c0c5e
Matt Withum <matt.withum@ectobox.com>
2017-09-28 16:44:57 -0400

    initial commit with some functions stuff

---
+++ Functions/HttpFunction.cs
@@ -0,0 +1,44 @@
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Functions
+{
+    public static class HttpFunction
+    {
+        private class SomeOtherClass
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        [FunctionName("HttpFunction")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        {
+            log.Info("C# HTTP trigger function processed a request THAT I MADE IN VS 2017.");
+
+            // parse query parameter
+            string name = req.GetQueryNameValuePairs()
+                .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
+                .Value;
+
+            // Get request body
+            dynamic data = await req.Content.ReadAsAsync<object>();
+
+            // Set name to query string or body data
+            name = name ?? data?.name;
+
+            var c1 = new SomeOtherClass { Id = 4, Name = "Four Guy" };
+
+            return name == null
+                ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
+                : req.CreateResponse(HttpStatusCode.OK, "Hey there " + name);
+        }
+    }
+}
